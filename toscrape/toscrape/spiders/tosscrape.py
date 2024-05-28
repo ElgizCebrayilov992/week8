4c599802364b6e02356b7df30f7338fd6f25bb43
ElgizCebrayilov992 <instgram.cominfi@gmail.com>
2024-05-28 13:46:41 +0400

    change

--- toscrape/toscrape/spiders/tosscrape.py
+++ toscrape/toscrape/spiders/tosscrape.py
@@ -18,3 +18,4 @@ class QuotesSpider(scrapy.Spider):
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
+# Comment atdim
